b722cc80a91d32df322c5a05bddfd64b04f88c3b
Sung-kyung <yoo960115@gmail.com>
2020-06-12 18:41:31 +0900

    상단바 로고추가

--- part/head.php
+++ part/head.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>블로그</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
     <link rel="stylesheet" href="/resource/common.css">
     <script src="/resource/common.js"></script>
 </head>
 <body>
 <div class="top-bar flex  flex-jc-c">
+    <div class="con flex height-100p">
+        <a href="/" class="logo flex flex-ai-c"><i class="fas fa-home"></i></a>
     <nav class="menu-box-1 flex con flex-jc-c ">
     <ul class="flex flex-jc-c ">
         <li class="flex-1-0-0 flex-jc-c"><a href="/" class="flex flex-ai-c">Home</a></li>
         <li class="flex-1-0-0 flex-jc-c"><a href="/list.php" class="flex flex-ai-c">Articles</a></li>
         <li class=" flex-1-0-0 flex flex-jc-c"><a href="/aboutMe.php" class="flex flex-ai-c">About Me</a></li>
         <li class="flex-1-0-0 flex flex-jc-c"><a href="#" class="flex flex-ai-c">SNS
-       </a></li>
+       </a>
+       <ul>
+                <li><a href="#">Instagram</a></li>
+                <li><a href="#">Github</a></li>
+            </ul></li>
     </ul></nav>
+    </div>
 </div>
