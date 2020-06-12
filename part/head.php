111bee717596d4dc81b4bc3260cf3b24123fca9d
Sung-kyung <yoo960115@gmail.com>
2020-06-12 16:29:28 +0900

    상단바 작업

---
+++ part/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>블로그</title>
+    <link rel="stylesheet" href="/resource/common.css">
+    <script src="/resource/common.js"></script>
+</head>
+<body>
+<div class="top-bar flex  flex-jc-c">
+    <nav class="menu-box-1 flex con flex-jc-c ">
+    <ul class="flex flex-jc-c ">
+        <li class="flex-1-0-0 flex-jc-c"><a href="/" class="flex flex-ai-c">Home</a></li>
+        <li class="flex-1-0-0 flex-jc-c"><a href="/list.php" class="flex flex-ai-c">Articles</a></li>
+        <li class=" flex-1-0-0 flex flex-jc-c"><a href="/aboutMe.php" class="flex flex-ai-c">About Me</a></li>
+        <li class="flex-1-0-0 flex flex-jc-c"><a href="#" class="flex flex-ai-c">SNS
+       </a></li>
+    </ul></nav>
+</div>
--- public/list.php
+++ public/list.php
@@ -1,7 +1,7 @@
 <?php
     include "../part/head.php";
 ?>
-    <h1 class="con">메인페이지입니다.</h1>
+    <h1 class="con">리스트입니다.</h1>
     
 <?php
     include "../part/foot.php";
