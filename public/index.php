f671f9f39b56596aa9771081271e66ca0753f2fd
Sung-kyung <yoo960115@gmail.com>
2020-06-19 17:06:41 +0900

    200619 수정중

--- public/index.php
+++ public/index.php
@@ -3,11 +3,9 @@
 ?>
 <div class="slider con">
     <div class="slides con">
-    <div style="background-image: url(/resource/img-2/banner-3.jpg);" class=" banner-1 active con"></div>
-    <div style="background-image: url(/resource/img-2/banner-7.jpg); " class="banner-2 con"></div>
-        <div style="background-image: url(/resource/img-2/Untitled-5.jpg); " class="banner-3 con imsg-box"></div>
- 
-
+        <div style="background-image: url(/resource/img-2/banner-3.jpg);" class="block banner-1 active con"></div>
+        <div style="background-image: url(/resource/img-2/banner-7.jpg); " class="block banner-2 con"></div>
+        <div style="background-image: url(/resource/img-2/banner-8.jpg); " class="block banner-3 con"></div>
     </div>
     <div class="side-bars con">
         <div>
@@ -20,10 +18,28 @@
         </div>
     </div>
 </div>
-<h1 class="con">Introduction</h1>
-<div class="line con"></div>
-<div class="avatar con"><img src="/resource/img/avatar.png" alt="">
-<div class="avatar-text">Level.26 / Bible / Student </div></div>
+
+<div class="intro con flex">
+    <div class="img-box-1 flex-1-0-0">
+            <div class="img height-100p" style="background-image:url(/resource/img-2/face.png)"></div>
+        </div>
+    <div class="img-box-2 flex-1-0-0">
+        <div class="height-50p">Introduction
+            <div class="line"></div>
+        </div>
+        <div class="img height-50p" style="background-image:url(/resource/img-2/banner-6.jpg)"></div>
+    </div>
+    
+</div>
+
+
+<div class="avatar con">
+    <img src="/resource/img/avatar.png" alt="">
+</div>
+
+<div class="avatar-text">Level.26 / Bible / Student </div>
+
+
 <?php
     include "../part/foot.php";
 ?>
