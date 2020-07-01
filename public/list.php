8f005ab93d93c5ff473815db87c657477c214f69
Sung-kyung <yoo960115@gmail.com>
2020-07-01 18:47:06 +0900

    왜이래 진짜..

--- public/list.php
+++ public/list.php
@@ -1,19 +1,46 @@
 <?php
     include "../part/head.php";
 ?>
-    <h1 class="list-title con">< Cafe Tour List ></h1>
-    
+<?php
+$dbConn = mysqli_connect("site17.blog.oa.gg","site17","sbs123414","site17",3306);
+
+$cateItemId = $_GET['cateItemId'];
+
+$sql = "
+SELECT *
+FROM article
+WHERE cateItemId = {$cateItemId}
+LIMIT 50
+";
+
+$rs = mysqli_query($dbConn,$sql);
+
+$articles = [];
+
+while ( $article = mysqli_fetch_assoc($rs) ) {
+    $articles[] = $article;
+}
+
+$sql2 = "
+SELECT `name`
+FROM cateItem
+WHERE id = {$cateItemId}
+";
+$rs2 = mysqli_query($dbConn, $sql2);
+$row = mysqli_fetch_assoc($rs2);
+?>
+
+<h1 class="list-title con"><?=$row['name']?></h1>
     
 <ul class="detail-list-box con block ">
-   
-   
-        <li class="detail-list"><div class="img-box"><img src="https://i.postimg.cc/pr3cZs3J/Kakao-Talk-20200623-023830701-1.jpg" alt=""></div>
-        <a href="/detail.php?id=1">갈마동 Eventide</a>
+    <?php foreach ($articles as $aa) { ?>
+    <li class="detail-list">
+        <div class="img-box">
+            <img src="https://i.postimg.cc/pr3cZs3J/Kakao-Talk-20200623-023830701-1.jpg" alt="">
+        </div>
+        <a href="/detail.php?id=<?=$aa['id']?>"><?=$aa['title']?></a>
     </li>
-        
-    <li class="detail-list-2">
-    <div class="img-box"><img src="https://i.postimg.cc/Px6hykXs/image-10.png" alt=""></div>
-    <a href="/detail.php?id=2">블로그 만들기</a></li>
+    <?php } ?>
 </ul>
 
 <?php
