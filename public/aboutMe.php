f99622ea04a260331c2b01d8d0500f580dcb747e
Sung-kyung <yoo960115@gmail.com>
2020-07-06 22:03:34 +0900

    어바웃미 수정

--- public/aboutMe.php
+++ public/aboutMe.php
@@ -2,27 +2,19 @@
     include "../part/head.php";
 ?>
 
-<div class="bg-aboutMe con">
-    <div class="intro-me"><img src="/resource/img-2/me1.jpg" alt=""></div>
-    <div class="intro-me-text">About Me Title</div>
-    <ul class="intro-me-content">
-        <li>Name : Sung-kyung / Emma</li>
-        <li>Age : 26</li>
-        <li>Hobby : </li>
-        <li>Nickname : Bible , 덕덕박사</li>
-        <li>Birth : 1996. 01. 15</li>
-        <li>Email : yoo960115@hanmail.net</li>
-        <li>Skill : </li>
-    </ul>
-    <div class="line-c"></div>
-    <div class="line-r"></div>
-
-    <div class="avatar con flex">
-        <img src="/resource/img/avatar.png" alt="" class="flex">
-        
+<div class="bg-me con">
+    <div class="me-circle"></div>
+    <img class="me" src="../resource/img-2/aboutMe.png" alt="">
+    <img class="dott" src="../resource/img-2/dott-sm.svg" alt="">
+    <div class="me-title-box">
+        <div class="me-title">Web Designer</div>
+        <div class="me-title-2">&</div>
+        <div class="me-title-3">Web Publisher</div>
     </div>
-    <div class="avatar-text flex">Level.26 / Bible / Student </div>
-
+    <div class="me-sub-box">
+        <div class="about-me">About Me</div>
+        <div class="about-me-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis vitae odio, quod repudiandae sequi minus fugiat mollitia facilis nostrum sint aliquam tempore eligendi exercitationem eaque provident quam illo! Odit!Reprehenderit voluptatibus obcaecati inventore nulla possimus similique maiores modi non, exercitationem quos, consequuntur repellendus accusantium nesciunt laborum velit, nam eaque ratione porro quae praesentium tempora aspernatur? Delectus accusamus deserunt distinctio.</div>
+    </div>
 </div>
 
 
