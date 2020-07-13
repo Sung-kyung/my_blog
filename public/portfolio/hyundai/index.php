4743294a1c131937995ebeaebc8bef37cab23d86
Sung-kyung <yoo960115@gmail.com>
2020-07-14 03:03:53 +0900

    0713 슬라이더 글씨추가작업중

--- public/portfolio/hyundai/index.php
+++ public/portfolio/hyundai/index.php
@@ -42,7 +42,8 @@
             </a>
         </div>
         <div class="search-ico cell-right">
-            <a href="" class="block" style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/ico_package_2x.png);">
+            <a href="" class="block"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/ico_package_2x.png);">
             </a>
         </div>
     </div>
@@ -54,15 +55,40 @@
     <div class="slide-box con slider-k height-full" data-autoplay="Y" data-autoplay-dir="left"
         data-autoplay-interval="4000">
         <div class="slides">
-            <div class="active"
-                style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/e032d779-80d0-43cb-bdb1-a6ba92e61cd8.resize_large.png);">
+            <div class="slide-1">
+                <div class="active"
+                    style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/e032d779-80d0-43cb-bdb1-a6ba92e61cd8.resize_large.png);">
+                </div>
+                <div class="text-1">
+                    <span>On View</span>
+                    <span>현대 HYUNDAI 50 Part II</span>
+                    <span>12 June - 19 July, 2020</span>
+                </div>
+
             </div>
-            <div
-                style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/ded83795-0f52-4f30-8ce5-0b2a615ed099.resize_large.png);">
+
+            <div class="slide-2">
+                <div
+                    style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/ded83795-0f52-4f30-8ce5-0b2a615ed099.resize_large.png);">
+                </div>
+                <div class="text-2">
+                    <span>news</span>
+                    <span>Kang Seung Lee 이강승 : No Space, Just A Place</span>
+                </div>
+
             </div>
-            <div
-                style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/78607676-1f78-41e6-bfb4-402d750bdfda.resize_large.png);">
+
+            <div class="slide-3">
+                <div
+                    style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/78607676-1f78-41e6-bfb4-402d750bdfda.resize_large.png);">
+                </div>
+                <div class="text-3">
+                    <span>essay</span>
+                    <span>Park Minjoon 박민준 : Story of Rapport</span>
+                </div>
+
             </div>
+
         </div>
     </div>
 
