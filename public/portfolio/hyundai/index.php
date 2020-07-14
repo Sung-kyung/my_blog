29957d1a58371a8a71789e1d26d74ca55d0434c9
Sung-kyung <yoo960115@gmail.com>
2020-07-14 18:41:21 +0900

    반응형으로 만들기 0714

--- public/portfolio/hyundai/index.php
+++ public/portfolio/hyundai/index.php
@@ -48,48 +48,80 @@
         </div>
     </div>
 
-
-
-
-
     <div class="slide-box con slider-k height-full" data-autoplay="Y" data-autoplay-dir="left"
         data-autoplay-interval="4000">
         <div class="slides">
-            <div class="slide-1">
-                <div class="active"
+            <div class="slide active">
+                <div
                     style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/e032d779-80d0-43cb-bdb1-a6ba92e61cd8.resize_large.png);">
                 </div>
-                <div class="text-1">
-                    <span>On View</span>
-                    <span>현대 HYUNDAI 50 Part II</span>
-                    <span>12 June - 19 July, 2020</span>
-                </div>
-
+                <section class="text">
+                    <span class="text-1-1 artist block">On View</span>
+                    <span class="text-1-2 title block">현대 HYUNDAI 50 Part II</span>
+                    <span class="text-1-3 date block">12 June - 19 July, 2020</span>
+                </section>
             </div>
 
-            <div class="slide-2">
+            <div class="slide">
                 <div
                     style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/ded83795-0f52-4f30-8ce5-0b2a615ed099.resize_large.png);">
                 </div>
-                <div class="text-2">
-                    <span>news</span>
-                    <span>Kang Seung Lee 이강승 : No Space, Just A Place</span>
-                </div>
-
+                <section class="text">
+                    <span class="text-2-1 artist block">news</span>
+                    <span class="text-2-2 title block">Kang Seung Lee 이강승 : No Space, Just A Place</span>
+                </section>
             </div>
 
-            <div class="slide-3">
+            <div class="slide">
                 <div
                     style="background-image:url(https://sung-kyung.github.io/img1/pf/hyundai/78607676-1f78-41e6-bfb4-402d750bdfda.resize_large.png);">
                 </div>
-                <div class="text-3">
-                    <span>essay</span>
-                    <span>Park Minjoon 박민준 : Story of Rapport</span>
-                </div>
-
+                <section class="text">
+                    <span class="text-3-1 artist block">essay</span>
+                    <span class="text-3-2 title block">Park Minjoon 박민준 : Story of Rapport</span>
+                </section>
             </div>
-
         </div>
+    </div>
+
+    <div class="post-list con">
+        <ul class="row">
+            <li class="cell ">
+                <a href="#" class="img-box">
+                    <img src="https://sung-kyung.github.io/img1/pf/hyundai/df73b845-3e4e-4e17-a503-ce2e5b778326.crop_middle.jpg"
+                        alt="">
+                    <span class="category block">News</span>
+                    <span class="post-title block">Ik-Joong Kang 강익중 : Gwanghwamun Arirang</span>
+                </a>
+            </li>
+            <li class="cell ">
+                <a href="#" class="img-box">
+                    <img src="https://sung-kyung.github.io/img1/pf/hyundai/86790e37-a55e-4215-b432-0bc3a23bdb15.crop_middle.jpg"
+                        alt="">
+                    <span class="category block">Books</span>
+                    <span class="post-title block">Chung Sang-Hwa 정상화 : Excavations, 1964–78</span>
+                </a>
+            </li>
+            <li class="cell ">
+                <a href="#" class="img-box">
+                    <img src="https://sung-kyung.github.io/img1/pf/hyundai/03a0f914-590d-48bd-880a-43321bc119ec.crop_middle.jpg"
+                        alt="">
+                    <span class="category block">News</span>
+                    <span class="post-title block">Choe U-Ram 최우람 : Custos Cavum</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="sticky-post con">
+        <a href="#">
+            <img src="https://sung-kyung.github.io/img1/pf/hyundai/30106a41-528d-4a64-bdc2-b556f990d497.resize_large.jpg" alt="">
+        </a>
+        <a href="#" class="post-text">
+            <span class="category block">Press</span>
+            <span class="title block">Minjung Kim 김민정 : In Conversation</span>
+            <span class="excerpt">“Ink and paper—they are like a couple. The ink is made by the smoke of the trees.”</span>
+        </a>
     </div>
 
 </body>
