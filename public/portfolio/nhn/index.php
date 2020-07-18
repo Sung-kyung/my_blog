96d6cd50a32f057c3ecd0d4b4263065ea4b8fa9f
Sung-kyung <yoo960115@gmail.com>
2020-07-18 21:14:52 +0900

    nhn 탑바까지

---
+++ public/portfolio/nhn/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NHN 모작</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700;900&family=Roboto:wght@400;700;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="/portfolio/nhn/index.css">
+    <script src="/portfolio/nhn/index.js"></script>
+</head>
+
+<body>
+    <div class="top-bar flex">
+        <div class="flex con flex-grow-1 flex-ai-c logo-box"><a href="#" class="logo flex"><img
+                    src="https://sung-kyung.github.io/img1/pf/nhn/logo_white.png" alt=""></a></div>
+        <div class="menu-box-1 flex con flex-ai-c flex-jc-e">
+            <ul class="flex">
+                <li class="flex"><a href="#">NHN</a></li>
+                <li class="flex"><a href="#">SERVICES</a></li>
+                <li class="flex"><a href="#">PR</a></li>
+                <li class="flex"><a href="#">IR</a></li>
+                <li class="flex"><a href="#">RECRUIT</a></li>
+                <li class="flex"><a href="#">INSIDE NHN</a></li>
+            </ul>
+        </div>
+        <div class="language flex con flex-ai-c ">
+            <ul class="flex">
+                <li><a href="#" class="kr">KR</a></li>
+                <li><a href="#" class="en">EN</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="slide-box">
+        <ul>
+            <li style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/main_01_3840.jpg);">
+                <div class="text-box">
+                    <h3 class="title">Play Hard, Work Smart!</h3>
+                    <p class="text">놀이가 작품이 되고 상상이 현실이 되는 곳</p>
+                </div>
+            </li>
+            <li style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/main_02_3840.jpg);">
+                <div class="text-box">
+                    <h3 class="title">Let the World Play with Us!</h3>
+                    <p class="text">더 넓은 세상, 더 많은 사람들이 더 큰 재미를 우리와 함께 나눕니다.</p>
+                </div>
+            </li>
+            <li style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/main_03_3840.jpg);">
+                <div class="text-box">
+                    <h3 class="title">Toward Art, Science & Tech</h3>
+                    <p class="text">새로운 세상을 만들기 위해 도전합니다</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="service-box con">
+        <ul class="service-list">
+            <li class="item hangame"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/sp_ent_2x_v2.png);"><a
+                    href="#"></a></li>
+            <li class="item payco"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/sp_ent_2x_v2.png);"><a
+                    href="#"></a></li>
+            <li class="item toast"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/sp_ent_2x_v2.png);"><a
+                    href="#"></a></li>
+            <li class="item comico"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/sp_ent_2x_v2.png);"><a
+                    href="#"></a></li>
+            <li class="item bugs"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/sp_ent_2x_v2.png);"><a
+                    href="#"></a></li>
+            <li class="item ticketlink"
+                style="background-image:url(https://sung-kyung.github.io/img1/pf/nhn/sp_ent_2x_v2.png);"><a
+                    href="#"></a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
