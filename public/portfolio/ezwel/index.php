920b2b9e48615752f2702001d2aaff6cd77b4108
Sung-kyung <yoo960115@gmail.com>
2020-07-25 03:06:19 +0900

    2페이지 추가

--- public/portfolio/ezwel/index.php
+++ public/portfolio/ezwel/index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>이지웰 모작</title>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="/portfolio/ezwel/index.css">
     <script src="/portfolio/ezwel/index.js"></script>
 </head>
+
 <body>
+    <!--탑바-->
     <div class="top-bar flex">
         <div class="logo-box flex flex-ai-c"><a href="#"></a></div>
         <div class="menu-box-1 flex flex-ai-c flex-grow-1">
@@ -63,44 +67,55 @@
         </div>
     </div>
 
-    
-   <div class="slide-box slider-k height-full" data-autoplay="Y" data-autoplay-dir="left" data-autoplay-interval="3000">
-    <div class="slides">
-        <div class="active" style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img02.jpg);">
-            <div class="text-info">
-                <p class="title">기업의 성공과<br>성장을 돕는</p>
-                <p class="text">Good Partner</p>
+    <!--탑 슬라이더-->
+    <div class="slide-box">
+        <div class="slider-k height-full" data-autoplay="Y" data-autoplay-dir="left" data-autoplay-interval="3000">
+            <div class="slides">
+                <div class="active"
+                    style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img02.jpg);">
+                    <div class="text-info">
+                        <p class="title">기업의 성공과<br>성장을 돕는</p>
+                        <p class="text">Good Partner</p>
+                    </div>
+                </div>
+                <div style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img03.jpg);">
+                    <div class="text-info">
+                        <p class="title">대한민국<br>복지기준을 만들어가는</p>
+                        <p class="text">Market Leader</p>
+                    </div>
+                </div>
+                <div style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img01.jpg);">
+                    <div class="text-info">
+                        <p class="title">일과 삶의 균형을<br>디자인하는</p>
+                        <p class="text">Life Styler</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img03.jpg);">
-            <div class="text-info">
-                <p class="title">대한민국<br>복지기준을 만들어가는</p>
-                <p class="text">Market Leader</p>
+
+            <div class="side-btns">
+                <div class="left-btn">
+                    <span>
+                    </span>
+                </div>
+                <div class="right-btn">
+                    <span>
+                    </span>
+                </div>
             </div>
-        </div>
-        <div style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img01.jpg);">
-            <div class="text-info">
-                <p class="title">일과 삶의 균형을<br>디자인하는</p>
-                <p class="text">Life Styler</p>
+
+            <div class="mouse">
+                <span></span>
             </div>
         </div>
     </div>
-    
-    <div class="side-btns">
-        <div class="left-btn">
-            <span>
-            </span>
-        </div>
-        <div class="right-btn">
-            <span>
-            </span>
-        </div>
+    <!--최신정보 페이지 -->
+    <div class="about-ezwel">
+        <h1 class="about-ezwel-title">
+            <span>ABOUT EZWEL</span>
+            <br>이지웰의<br>최신 정보입니다.
+        </h1>
     </div>
-    
-    <div class="mouse">
-        <span></span>
-    </div>
-</div>
-    
+
 </body>
+
 </html>
