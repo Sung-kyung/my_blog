610dfd13354eb2ee9bad3136218a38df90a22d5c
Sung-kyung <yoo960115@gmail.com>
2020-07-28 18:50:42 +0900

    추가추가

--- public/portfolio/ezwel/index.php
+++ public/portfolio/ezwel/index.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
-<html lang="ko">
+<html lang="ko" data-fullpage-index="0">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>이지웰 모작</title>
+    <script src="https://alvarotrigo.com/fullPage/fullpage.js"></script>
+    <link rel="stylesheet" href="https://alvarotrigo.com/fullPage/fullpage.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Slick 불러오기 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
         rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="/portfolio/ezwel/index.css">
     <script src="/portfolio/ezwel/index.js"></script>
 </head>
@@ -66,95 +72,140 @@
             <div class="bg-box"></div>
         </div>
     </div>
+    <div id="fullpage">
+        <div class="section">
+            <!--탑 슬라이더-->
+            <div class="slide-box">
+                <div class="slider-k height-full" data-autoplay="Y" data-autoplay-dir="right"
+                    data-autoplay-interval="3000">
+                    <div class="slides">
+                        <div>
+                            <div class="bg"
+                                style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img02.jpg);">
+                            </div>
+                            <div class="text-info">
+                                <p class="title">기업의 성공과<br>성장을 돕는</p>
+                                <p class="text">Good Partner1</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bg"
+                                style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img03.jpg);">
+                            </div>
+                            <div class="text-info">
+                                <p class="title">대한민국<br>복지기준을 만들어가는</p>
+                                <p class="text">Market Leader2</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bg"
+                                style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img01.jpg);">
+                            </div>
+                            <div class="text-info">
+                                <p class="title">일과 삶의 균형을<br>디자인하는</p>
+                                <p class="text">Life Styler3</p>
+                            </div>
+                        </div>
+                    </div>
 
-    <!--탑 슬라이더-->
-    <div class="slide-box">
-        <div class="slider-k height-full" data-autoplay="Y" data-autoplay-dir="right" data-autoplay-interval="3000">
-            <div class="slides">
-                <div>
-                    <div class="bg" style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img02.jpg);"></div>
-                    <div class="text-info">
-                        <p class="title">기업의 성공과<br>성장을 돕는</p>
-                        <p class="text">Good Partner1</p>
+                    <div class="side-btns">
+                        <div class="left-btn">
+                            <span>
+                            </span>
+                        </div>
+                        <div class="right-btn">
+                            <span>
+                            </span>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <div class="bg" style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img03.jpg);"></div>
-                    <div class="text-info">
-                        <p class="title">대한민국<br>복지기준을 만들어가는</p>
-                        <p class="text">Market Leader2</p>
-                    </div>
-                </div>
-                <div>
-                    <div class="bg" style="background-image:url(https://sung-kyung.github.io/img1/pf/ezwel/main-slider-img01.jpg);"></div>
-                    <div class="text-info">
-                        <p class="title">일과 삶의 균형을<br>디자인하는</p>
-                        <p class="text">Life Styler3</p>
+
+                    <div class="mouse">
+                        <span></span>
                     </div>
                 </div>
             </div>
-
-            <div class="side-btns">
-                <div class="left-btn">
-                    <span>
-                    </span>
-                </div>
-                <div class="right-btn">
-                    <span>
-                    </span>
+        </div>
+        <div class="section">
+            <!--최신정보 페이지 -->
+            <div class="about-bg"></div>
+            <div class="about-ezwel">
+                <div class="con-box">
+                    <div class="info-box">
+                        <h1 class="about-ezwel-title">
+                            <span>ABOUT EZWEL</span>
+                            <br>이지웰의<br>최신 정보입니다.
+                        </h1>
+                        <div class="stock-box">
+                            <div class="stock">
+                                <span class="stock-info">주가정보</span>
+                                <p class="up-down">350</p>
+                                <p class="num">12,000</p>
+                                <span class="date">2020/07/25 00:51:32</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-list flex con">
+                        <div class="news flex">
+                            <span class="list-title">NEWS</span>
+                            <div class="news-slide-box slider-div">
+                                <div class="news-slides">
+                                    <span class="news-category">보도자료</span>
+                                    <p class="news-title">이지웰, "1Q 전년 동기대비 연결 영업이익 45% 증가"</p>
+                                    <p class="news-content">2020-05-15</p>
+                                </div>
+                                <div class="news-slides">
+                                    <span class="news-category">보도자료</span>
+                                    <p class="news-title">이지웰, 지역화폐사업을 위해 KT와 전략적 제휴 체결</p>
+                                    <p class="news-content">2020-06-29</p>
+                                </div>
+                                <div class="news-slides">
+                                    <span class="news-category">보도자료</span>
+                                    <p class="news-title">이지웰, 온누리전통시장몰 ‘모바일 온누리상품권’ 결제 도입</p>
+                                    <p class="news-content">2020-06-10</p>
+                                </div>
+                            </div>
+                            <div class="news-side-btns">
+                                <div class="left-btn">
+                                    <span>
+                                    </span>
+                                </div>
+                                <div class="right-btn">
+                                    <span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="financial-box flex">
+                            <div class="financial">
+                                <p class="financial-title">재무정보</p>
+                                <p class="financial-content">분기/연도별<br>재무정보입니다.</p>
+                            </div>
+                        </div>
+                        <div class="notice-box flex">
+                            <div class="notice">
+                                <span>공시</span>
+                                <div>
+                                    <p class="notice-content">주식등의대량보유상황보고서(일반)</p>
+                                    <span class="notice-date">2020-07-24</span>
+                                </div>
+                                <div>
+                                    <p class="notice-content">교환청구권행사</p>
+                                    <span class="notice-date">2020-07-24</span>
+                                </div>
+                                <div>
+                                    <p class="notice-content">자기주식처분결과보고서</p>
+                                    <span class="notice-date">2020-07-22</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="mouse">
-                <span></span>
-            </div>
+        </div>
+        <div class="section">
+            3
         </div>
     </div>
-    <!--최신정보 페이지 -->
-    <div class="about-ezwel">
-        <div class="info-box">
-            <h1 class="about-ezwel-title">
-                <span>ABOUT EZWEL</span>
-                <br>이지웰의<br>최신 정보입니다.
-            </h1>
-            <div class="stock-box">
-                <div class="stock">
-                    <span class="stock-info">주가정보</span>
-                    <p class="up-down">350</p>
-                    <p class="num">12,000</p>
-                    <span class="date">2020/07/25 00:51:32</span>
-                </div>
-            </div>
-        </div>
-        <div class="about-list flex">
-            <div class="news flex"></div>
-            <div class="financial-box flex">
-                <div class="financial">
-                    <p class="financial-title">재무정보</p>
-                    <p class="financial-content">분기/연도별<br>재무정보입니다.</p>
-                </div>
-            </div>
-            <div class="notice-box flex">
-                <div class="notice">
-                    <span>공시</span>
-                    <div>
-                        <p class="notice-content">주식등의대량보유상황보고서(일반)</p>
-                        <span class="notice-date">2020-07-24</span>
-                    </div>
-                    <div>
-                        <p class="notice-content">교환청구권행사</p>
-                        <span class="notice-date">2020-07-24</span>
-                    </div>
-                    <div>
-                        <p class="notice-content">자기주식처분결과보고서</p>
-                        <span class="notice-date">2020-07-22</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="about-bg"></div>
-    </div>
-
 </body>
 
 </html>
