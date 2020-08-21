07984d59b7f3ad93bd0db4695679f7216bd1f3d9
Sung-kyung <yoo960115@gmail.com>
2020-08-21 18:39:49 +0900

    포폴 페이지 추ㅏㄱ

---
+++ public/portfolio2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>유성경 포트폴리오</title>
+</head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="/portfolio2/index.js"></script>
+<link rel="stylesheet" href="/portfolio2/index.css">
+<body>
+    
+</body>
+</html>
