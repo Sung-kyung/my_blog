2ffc6c1e99a4034dd93e3eedb823755332bd6b9e
Sung-kyung <yoo960115@gmail.com>
2020-08-25 18:49:34 +0900

    sdfsdf

--- public/portfolio2/index.php
+++ public/portfolio2/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +10,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="/portfolio2/index.js"></script>
 <link rel="stylesheet" href="/portfolio2/index.css">
+
 <body>
-    
+
 </body>
+
 </html>
