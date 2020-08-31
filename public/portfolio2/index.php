a69c7d3bad0dd9af66f9d803283b4b08e5ba16d5
Sung-kyung <yoo96011@gmail.com>
2020-09-01 03:13:51 +0900

    receipt

--- public/portfolio2/index.php
+++ public/portfolio2/index.php
@@ -243,8 +243,8 @@
         <div class="img-box-3 cell">
             <div class="monitor">
                 <img src="https://sung-kyung.github.io/img1/pf/portfolio/monitor.png" alt="">
-                <div class="img-box-2"></div>     
-        </div>
+                <div class="img-box-2"></div>
+            </div>
         </div>
         <div class="text-box-3 cell-right">
             <div class="title cell-right">DESIGN</div>
@@ -254,26 +254,127 @@
                 <div class="creat-box"></div>
                 <div class="title-ko-box"></div>
             </div>
-              <div class="slide-2">
+            <div class="slide-2">
                 <div class="owl-2 owl-carousel owl-theme">
-                    <div class="item item-2-1" data-no2="01" data-title-en="Tomato Infographic" data-creat="Creative _ Copy" data-title-ko="토마토 인포그래픽" data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/tomato.png" onclick="Slider1__itemClick2(this);" ></div>
+                    <div class="item item-2-1" data-no2="01" data-title-en="Tomato Infographic"
+                        data-creat="Creative _ Copy" data-title-ko="토마토 인포그래픽"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/tomato.png"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="02"  data-title-en="WaterMelon Juice" data-creat="Creative _ Copy" data-title-ko="수박주스" data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/watermelon.jpg" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="02" data-title-en="WaterMelon Juice" data-creat="Creative _ Copy"
+                        data-title-ko="수박주스"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/watermelon.jpg"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="03"  data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/illom.jpg" data-title-en="Illom bosten Sofa" data-creat="Creative _ Redesign" data-title-ko="일룸 보스턴 소파" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="03"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/illom.jpg"
+                        data-title-en="Iloom bosten Sofa" data-creat="Creative _ Redesign" data-title-ko="일룸 보스턴 소파"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="04"  data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/orange.jpg" data-title-en="Orange Juice" data-creat="Creative _ Redesign" data-title-ko="오렌지 주스" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="04"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/orange.jpg"
+                        data-title-en="Orange Juice" data-creat="Creative _ Redesign" data-title-ko="오렌지 주스"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="05"  data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/social.jpg" data-title-en="Social Media" data-creat="Creative _ Copy" data-title-ko="소셜 미디어"  onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="05"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/social.jpg"
+                        data-title-en="Social Media" data-creat="Creative _ Copy" data-title-ko="소셜 미디어"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="06" data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/covid.jpg" data-title-en="Social Distance" data-creat="Creative _ Redesign" data-title-ko="사회적 거리두기" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="06"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/covid.jpg"
+                        data-title-en="Social Distance" data-creat="Creative _ Redesign" data-title-ko="사회적 거리두기"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="07"  data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/dior.jpg" data-title-en="Dior Addict" data-creat="Creative _ Redesign" data-title-ko="디올 어딕트" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="07"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/dior.jpg"
+                        data-title-en="Dior Addict" data-creat="Creative _ Redesign" data-title-ko="디올 어딕트"
+                        onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="08"  data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/africa.jpg" data-title-en="Africa Festival Poster" data-creat="Creative _ Redesign" data-title-ko="아프리카 페스티벌 포스터" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="08"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/africa.jpg"
+                        data-title-en="Africa Festival Poster" data-creat="Creative _ Redesign"
+                        data-title-ko="아프리카 페스티벌 포스터" onclick="Slider1__itemClick2(this);"></div>
 
-                    <div class="item" data-no2="09"  data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/travel.gif" data-title-en="Time To Travel" data-creat="Creative _ Copy" data-title-ko="여행 일러스트" onclick="Slider1__itemClick2(this);"></div>
+                    <div class="item" data-no2="09"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/travel.gif"
+                        data-title-en="Time To Travel" data-creat="Creative _ Copy" data-title-ko="여행 일러스트"
+                        onclick="Slider1__itemClick2(this);"></div>
+                        
+                        <div class="item" data-no2="10"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/diptyque.jpg"
+                        data-title-en="Diptyque" data-creat="Creative _ Redesign" data-title-ko="딥디크 향수배너"
+                        onclick="Slider1__itemClick2(this);"></div>
+
+                        <div class="item" data-no2="11"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/twosome.jpg"
+                        data-title-en="Twosome Summer" data-creat="Creative _ Copy" data-title-ko="투썸 플레이스"
+                        onclick="Slider1__itemClick2(this);"></div>
+
+                        <div class="item" data-no2="12"
+                        data-img2-url="https://sung-kyung.github.io/img1/pf/portfolio/domino.jpg"
+                        data-title-en="Domino Pizza" data-creat="Creative _ Redesign" data-title-ko="도미노 피자 배너"
+                        onclick="Slider1__itemClick2(this);"></div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <div class="page-4">
+        <div class="receipt-box"
+            style="background-image:url(https://sung-kyung.github.io/img1/pf/portfolio/receipt.png);">
+            <div class="profile-txt-box">
+                <div class="name">Sung Kyung</div>
+                <div class="profile">PROFILE</div>
+                <div class="profile-cont">
+                    <div>Name : 유성경</div>
+                    <div>Email : yoo960115@gmail.com</div>
+                    <div>Blog : https://site17.blog.oa.gg</div>
+                    <div>Wiki : https://to2.kr/bma</div>
+                    <span>#Web Design <br> #Web Publisher</span>
+                </div>
+                <div class="edu">
+                    <div class="education">Education</div>
+                    <div class="edu-1">2020.04 ~ 2020.10</div>
+                    <div class="edu-1">2014.03 ~</div>
+                    <div class="edu-1">2011.03 ~ 2014.02</div>
+                    <div class="certificate">Certificate</div>
+                    <div class="cer">운전면허 2종 보통</div>
+                    <div class="cer">컴퓨터 활용능력 1급</div>
+                    <div class="cer">웹 디자인 기능사 필기</div>
+                    <span class="sbs">대전 SBS 아카데미 컴퓨터학원 <br>웹 과정 수료 </span>
+                    <span class="hannam">한남대학교 컨벤션호텔경영 / <br>경영학과 졸업</span>
+                    <span class="high">순천매산여자고등학교 졸업</span>
+                </div>
+                <div class="skill">
+                    <div class="skill-title">SKILL</div>
+                    <div class="html flex flex-jc-sb skill-cont">
+                        <div class="flex">HTML</div>
+                        <div class="flex progress"></div>
+                        <div class="flex per">90%</div>
+                    </div>
+                    <div class="css flex flex-jc-sb skill-cont">
+                        <div class="flex">CSS</div>
+                        <div class="flex progress"></div>
+                        <div class="flex per">00%</div>
+                    </div>
+                    <div class="java flex flex-jc-sb skill-cont">
+                        <div class="flex">JavaScript</div>
+                        <div class="flex progress"></div>
+                        <div class="flex per">00%</div>
+                    </div>
+                    <div class="photo flex flex-jc-sb skill-cont">
+                        <div class="flex">Photoshop</div>
+                        <div class="flex progress"></div>
+                        <div class="flex per">00%</div>
+                    </div>
+                    <div class="illu flex flex-jc-sb skill-cont">
+                        <div class="flex">Illustrator</div>
+                        <div class="flex progress"></div>
+                        <div class="flex per">00%</div>
+                    </div>
+                </div>
+                <div class="thank">THANK YOU !</div>
+                <div class="feedback">Please Leave Your Feedback At Bottom</div>
             </div>
         </div>
     </div>
