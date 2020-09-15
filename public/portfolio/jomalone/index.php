8ae8897da1590b2e4db724d806857dcc1590a47e
Sung-kyung <yoo96011@gmail.com>
2020-09-16 03:15:32 +0900

    jomalone

---
+++ public/portfolio/jomalone/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jo Malone</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/portfolio/jomalone/index.css">
+    <script src="/portfolio/jomalone/index.js"></script>
+</head>
+
+<body>
+    <div class="page-1">
+        <div class="top-bar flex flex-jc-sb flex-ai-c">
+            <div class="flex logo"><img src="https://sung-kyung.github.io/img1/pf/jomalone/logo.png" alt=""></div>
+            <div class="flex menu-ico">
+                <div class="btn-toggle-mobile-side-bar">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        <video src="https://sung-kyung.github.io/img1/pf/jomalone/videoplayback.mp4" autoplay="autoplay" loop="loop"
+            muted="muted"></video>
+    </div>
+    <div class="page-2">
+        <div class="con page-2-box">
+            <div class="ancient">Ancient Airborne Garden</div>
+            <div class="description">고대 공중정원의 아름다운 전설에 영감을 받은<br>
+조 말론 런던의 새로운 컬렉션, 로스트 인 원더.</div>
+        <a href="#" class="more-view">MORE VIEW &nbsp;&nbsp;></a>
+        </div>
+    </div>
+
+</body>
+
+</html>
