c87afc46b43cb1ac60674e6a9ffb64183de6d5e0
Sung-kyung <yoo96011@gmail.com>
2020-10-16 18:31:40 +0900

    응?

---
+++ public/portfolio/pearlabyss/index.js
@@ -0,0 +1,45 @@
+  //메인 슬라이더
+  function Slider__init() {
+    $(".slider-div").slick({
+      slide: "div", //슬라이드 되어야 할 태그 ex) div, li
+      infinite: true, //무한 반복 옵션
+      slidesToShow: 1, // 한 화면에 보여질 컨텐츠 개수
+      slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
+      speed: 800, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
+      arrows: true, // 옆으로 이동하는 화살표 표시 여부
+      dots: true, //크롤바 아래 점으로 페이지네이션 여부
+      autoplay: true, // 자동 스크롤 사용 여부
+      autoplaySpeed: 3000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
+      draggable: true //드래그 가능 여부
+    });
+  }
+
+//menu hover  
+function hoverBar__init() {
+    $(".hover-bar-nav .hover-bar-menu").mouseenter(function () {
+      var $this = $(this);
+  
+      var menuWidth = $this.width();
+      var menuPadding = ($this.innerWidth() - $this.width()) / 2;
+      var menuPosition = $this.position().left;
+  
+      var $hoverBar = $(".hover-bar");
+  
+      $hoverBar.css("width", menuWidth + "px");
+      $hoverBar.css("left", menuPosition + menuPadding + "px");
+    });
+  
+    $(".hover-bar-nav").mouseleave(function () {
+      var $this = $(this);
+      var $hoverBar = $(".hover-bar");
+      $hoverBar.css("width", "");
+      $hoverBar.css("left", "");
+    });
+  }
+
+  
+  $(function(){
+    Slider__init();
+    hoverBar__init();
+  });
+  
---
+++ public/portfolio/pearlabyss/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>펄어비스</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Slick 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="/portfolio/pearlabyss/index.css">
+    <script src="/portfolio/pearlabyss/index.js"></script>
+</head>
+
+<body>
+    <!--탑바-->
+    <div class="top-bar">
+        <div class="top-border">
+            <div class="top-inner con flex flex-jc-sb">
+                <div class="logo-box flex"></div>
+                <div class="menu-box-1 hover-bar-box flex">
+                    <ul class="hover-bar-nav flex flex-ai-c">
+                        <li class="hover-bar-menu"><span>소개</span>
+                            <ul class="sub-menu">
+                                <li><a href="#">펄어비스 소개</a></li>
+                                <li><a href="#">연구소</a></li>
+                            </ul>
+                        </li>
+                        <li class="hover-bar-menu"><span>게임</span>
+                            <ul class="sub-menu">
+                                <li><a href="#">검은사막</a></li>
+                                <li><a href="#">검은사막 모바일</a></li>
+                                <li><a href="#">섀도우 아레나</a></li>
+                                <li><a href="#">붉은사막</a></li>
+                                <li><a href="#">도깨비</a></li>
+                                <li><a href="#">플랜 8</a></li>
+                            </ul>
+                        </li>
+                        <li class="hover-bar-menu"><span>미디어</span>
+                            <ul class="sub-menu">
+                                <li><a href="#">보도자료</a></li>
+                                <li><a href="#">미디어 행사</a></li>
+                            </ul>
+                        </li>
+                        <li class="hover-bar-menu"><span>문화</span>
+                            <ul class="sub-menu">
+                                <li><a href="#">기업문화</a></li>
+                                <li><a href="#">복지</a></li>
+                                <li><a href="#">펄어비스인</a></li>
+                                <li><a href="#">펄어비스 소식</a></li>
+                            </ul>
+                        </li>
+                        <li class="hover-bar-menu"><span>채용</span>
+                            <ul class="sub-menu">
+                                <li><a href="#">채용소개</a></li>
+                                <li><a href="#">지원하기</a></li>
+                            </ul>
+                        </li>
+                        <li class="hover-bar-menu"><span>투자정보</span>
+                            <ul class="sub-menu">
+                                <li><a href="#">기업지배구조</a></li>
+                                <li><a href="#">주가정보</a></li>
+                                <li><a href="#">재무정보</a></li>
+                                <li><a href="#">공시·공고</a></li>
+                                <li><a href="#">IR 자료</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="hover-bar"></div>
+                </div>
+                <div class="lang flex flex-ai-c">
+                    <span>KOR</span><span>ENG</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--메인 슬라이더-->
+    <div class="bn-bar">
+	  	<div class="slider-div">
+		    <div class="bn1 bn"></div>
+		    <div class="bn2 bn"></div>
+		    <div class="bn3 bn"></div>
+	  	</div>
+	</div>
+
+</body>
+
+</html>
